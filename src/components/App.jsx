2bfb1de80fbbcb45723de94d7bd45604a3542ade
Cuauhtemoc Cabrera <cabrera.cuau@gmail.com>
2025-05-25 00:01:55 -0600

    feat: setup project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../scss/components/_app.scss'
+
+function App() {
+  
+  return (
+    <>
+    <h1>CV Application</h1>
+    </>
+  )
+}
+
+export default App
